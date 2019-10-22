ce886a50494af13416d11c61fce4c286df973ae8
Evan Schweitzer <eschwei2@masonlive.gmu.edu>
2019-10-22 14:43:52 -0500

    proxy server started

--- server/index.js
+++ server/index.js
@@ -1,34 +1,15 @@
 const express = require('express');
 const path = require('path')
+const HTML = path.join(__dirname, '../client')
 const app = express();
-const port = 3042;
-const connection = require("./database/db");
-const SEARCH_BAR_DIST_DIR = path.join(__dirname, '../searchbar-dist');
-const SEARCH_BAR_HTML_FILE = path.join(SEARCH_BAR_DIST_DIR, 'index.html')
-const bodyParser = require('body-parser');
-const mockResponse = {
-    foo: 'bar',
-    bar: 'foo'
-};
+const port = 3099;
 
-// app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(express.static(SEARCH_BAR_DIST_DIR));
+app.use(express.static(HTML));
 
 app.get('/', (req, res) =>{
-    res.send(SEARCH_BAR_HTML_FILE);
+    res.send(HTML);
 });
 
-app.post('/autocomplete', (req, res) => {
-    connection.getOptions(req.body, (products) => {
-        res.send(products)
-    })
-})
-
-app.get('/categories', (req, res) =>
-    connection.getCategories((categories) => {
-        res.send(categories)
-    }))
 app.listen(port, function () {
     console.log('App listening on port: ' + port);
 });
